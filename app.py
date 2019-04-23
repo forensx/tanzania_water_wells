6d9a5b24ace366c04a8e503d496acb151297d314
AniketPant02 <aniketpant@jefcoed.com>
2019-04-23 01:39:45 -0500

    Worked on Home page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import os
+#Imports
 
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -7,12 +8,14 @@ from flask_dropzone import Dropzone
 import pandas as pd
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
+#Initialize App
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(SECRET_KEY='dev')
+if __name__ == '__main__':
+   app.run(debug = True)
 
+#App Routes
 
-# a simple page that says hello
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -26,24 +29,12 @@ def uploadpage():
     return render_template("upload.html")
 
 """
+Analysis Tab TBD
+
 @app.route('/analysis')
 def analysis()
     return render_template("analysis.html")
 """
-
-app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'upload'),
-    # Flask-Dropzone config:
-    DROPZONE_ALLOWED_FILE_CUSTOM= True,
-    DROPZONE_ALLOWED_FILE_TYPE= '.csv',
-    DROPZONE_MAX_FILE_SIZE=100,
-    DROPZONE_MAX_FILES=30,
-    DROPZONE_PARALLEL_UPLOADS=3,  # set parallel amount
-    DROPZONE_UPLOAD_MULTIPLE=True,  # enable upload multiple
-)
-
-dropzone = Dropzone(app)
-
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
@@ -57,6 +48,16 @@ def upload():
 
     return render_template('index.html')
 
-if __name__ == '__main__':
-   app.run(debug = True)
+#Dropzone Config
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'upload'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_CUSTOM= True,
+    DROPZONE_ALLOWED_FILE_TYPE= '.csv',
+    DROPZONE_MAX_FILE_SIZE=100,
+    DROPZONE_MAX_FILES=30,
+    DROPZONE_PARALLEL_UPLOADS=3,  # set parallel amount
+    DROPZONE_UPLOAD_MULTIPLE=True,  # enable upload multiple
+)
 
+dropzone = Dropzone(app)
